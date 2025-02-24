6a686a11ff5d3d29aa6651f61d6312c4c4b50592
Tony Le <git@tonyle.com>
2025-02-24 11:28:09 +1100

    Upgrade to Tailwind v4

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -79,7 +79,7 @@ export const Editor = ({
         />
       </div>
 
-      <Separator className="!my-8" />
+      <Separator className="my-8!" />
 
       <div className="flex items-center justify-between">
         <h2 className="font-semibold">Highlights</h2>
